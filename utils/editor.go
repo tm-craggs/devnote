b6cdb8baf3803171a629eb8d2d3225891e847a45
Tom Craggs <tom.craggs@protonmail.com>
2025-07-15 17:27:20 +0100

    Feature: OpenEditor function takes in a file path and opens it in the default text editor

--- utils/editor.go
+++ utils/editor.go
@@ -1,1 +1,21 @@
 package utils
+
+import (
+	"os"
+	"os/exec"
+)
+
+func OpenEditor(filename string) error {
+	editor := os.Getenv("EDITOR")
+	if editor == "" {
+		// fallback editor
+		editor = "nano"
+	}
+
+	cmd := exec.Command(editor, filename)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	return cmd.Run()
+}
