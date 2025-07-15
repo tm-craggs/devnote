fe08541dabbc5bdc91356429cf09fac3b69eb34b
Tom Craggs <tom.craggs@protonmail.com>
2025-07-15 15:06:01 +0100

    Inital commit

---
+++ main.go
@@ -0,0 +1,17 @@
+/*
+Copyright © 2025 Tom Craggs <tom.craggs@protonmail.com>
+License: GPL 3.0
+
+devlog is free software: you can redistribute and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of the License or any later version.
+
+You should have received a copy of the GNU General Public License along with this program.
+If not, see <https://www.gnu.org/licenses/gpl-3.0.html>
+*/
+package main
+
+import "github.com/tm-craggs/devnote/cmd"
+
+func main() {
+	cmd.Execute()
+}
